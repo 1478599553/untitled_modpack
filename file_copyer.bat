fe874c2517d5023839fcd01f030f6e21eb29f120
Xiaoming <1478599553@qq.com>
2021-08-26 22:37:45 +0800

    upd

--- file_copyer.bat
+++
@@ -1,5 +0,0 @@
-xcopy F:\GRASS_CRAFT\.minecraft\config\*.* F:\GRASS_CRAFT\EDITED_FILES\GRASS_CRAFT\config /E
-xcopy F:\GRASS_CRAFT\.minecraft\crash-reports\*.* F:\GRASS_CRAFT\EDITED_FILES\GRASS_CRAFT\crash-reports /E
-xcopy F:\GRASS_CRAFT\.minecraft\resources\*.* F:\GRASS_CRAFT\EDITED_FILES\GRASS_CRAFT\resources /E
-xcopy F:\GRASS_CRAFT\.minecraft\scripts\*.* F:\GRASS_CRAFT\EDITED_FILES\GRASS_CRAFT\scripts /E
-xcopy F:\GRASS_CRAFT\.minecraft\structures\*.* F:\GRASS_CRAFT\EDITED_FILES\GRASS_CRAFT\structures /E
